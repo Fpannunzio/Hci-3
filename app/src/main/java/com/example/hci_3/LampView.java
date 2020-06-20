691af7959476548577373ee89c7a837179fb4154
Ignacio Vazquez <igvazquez@itba.edu.ar>
2020-06-20 18:03:13 -0300

    lamp card y no se que otras cosas pq soy tarado y comiteo muchas cosas

---
+++ app/src/main/java/com/example/hci_3/LampView.java
@@ -0,0 +1,78 @@
+package com.example.hci_3;
+
+import android.content.Context;
+import android.transition.AutoTransition;
+import android.transition.TransitionManager;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import androidx.cardview.widget.CardView;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import com.example.hci_3.api.Device;
+import com.skydoves.colorpickerview.ColorPickerView;
+import com.skydoves.colorpickerview.listeners.ColorListener;
+
+public class LampView extends DeviceView {
+
+    private CardView cardView;
+    private ConstraintLayout expandableLayout;
+    private ImageButton extendBtn;
+    private TextView mDevName;
+    private ColorPickerView colorPickerView;
+    private View currentColor;
+
+    public LampView(Context context) {
+        super(context);
+    }
+
+    public LampView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public LampView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void init(Context context) {
+        super.init(context);
+        LayoutInflater.from(context).inflate(R.layout.lamp_view, this, true);
+
+        cardView = findViewById(R.id.cardView);
+        expandableLayout = findViewById(R.id.expandableLayout);
+        extendBtn = findViewById(R.id.expandBtn);
+        mDevName = findViewById(R.id.door_name);
+        colorPickerView = findViewById(R.id.colorPickerView);
+    }
+
+    @Override
+    public void setDevice(Device device) {
+        super.setDevice(device);
+
+//        mDevName.setText(getParsedName(device.getName()));
+
+        colorPickerView.setColorListener(new ColorListener() {
+            @Override
+            public void onColorSelected(int color, boolean fromUser) {
+                currentColor = findViewById(R.id.currentColor);
+                currentColor.setBackgroundColor(color);
+            }
+        });
+
+        extendBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (expandableLayout.getVisibility() == View.GONE){
+                    TransitionManager.beginDelayedTransition(cardView, new AutoTransition());
+                    expandableLayout.setVisibility(View.VISIBLE);
+                    // Falta rotar la flecha
+                } else {
+                    TransitionManager.beginDelayedTransition(cardView, new AutoTransition());
+                    expandableLayout.setVisibility(View.GONE);
+                }
+            }
+        });
+    }
+}
