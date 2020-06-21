e872f1f54af26c18ceb94a95018af19137abb575
Tobias Brandy <tobiasbrandy@hotmail.com>
2020-06-21 12:48:58 -0300

    Broadcast

---
+++ app/src/main/java/com/example/hci_3/NotificationBroadcastReceiver.java
@@ -0,0 +1,12 @@
+package com.example.hci_3;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class NotificationBroadcastReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // Armar notificaciones
+    }
+}
