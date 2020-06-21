c86916360bd01f80c7de99732f6804e4c2d36f3a
faus <fpannunzio@itba.edu.ar>
2020-06-20 23:43:44 -0300

    Vacuum, falta ver como hacer lo de ubicacion

--- app/src/main/java/com/example/hci_3/VacuumView.java
+++ app/src/main/java/com/example/hci_3/VacuumView.java
@@ -6,6 +6,7 @@ import android.transition.TransitionManager;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
 import android.widget.Spinner;
@@ -17,13 +18,20 @@ import androidx.lifecycle.LiveData;
 
 import com.example.hci_3.api.Device;
 
+import com.example.hci_3.api.DeviceStates.VacuumState;
+import com.google.android.material.button.MaterialButtonToggleGroup;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class VacuumView extends DeviceView {
 
-    private TextView mDevName;
+    private TextView mDevName, mState, mLocation;
     private CardView cardView;
     private ConstraintLayout expandableLayout;
     private ImageButton extendBtn;
-    private Spinner locationSpinner;
+    private Spinner mSpinner;
+    private MaterialButtonToggleGroup mStateGroup, mModeGroup;
     private ArrayAdapter<CharSequence> locationAdapter;
 
     public VacuumView(Context context) {
@@ -43,11 +51,15 @@ public class VacuumView extends DeviceView {
         super.init(context);
         LayoutInflater.from(context).inflate(R.layout.vacuum_view, this, true);
 
-        mDevName = findViewById(R.id.ac_name);
+        mDevName = findViewById(R.id.vacuum_name);
+        mLocation = findViewById(R.id.vacuum_location);
+        mState = findViewById(R.id.onStateVacuum);
         cardView = findViewById(R.id.cardView);
         expandableLayout = findViewById(R.id.expandableLayout);
         extendBtn = findViewById(R.id.expandBtn);
-        locationSpinner = findViewById(R.id.locationSpinner);
+        mSpinner = findViewById(R.id.locationSpinner);
+        mStateGroup = findViewById(R.id.vacuum_onstate_group);
+        mModeGroup = findViewById(R.id.vacuum_mode_group);
         locationAdapter = ArrayAdapter.createFromResource(context, R.array.vacuum_location, android.R.layout.simple_spinner_item);
     }
 
@@ -55,7 +67,7 @@ public class VacuumView extends DeviceView {
     public void setDevice(LiveData<Device> device) {
         super.setDevice(device);
 
-        locationSpinner.setAdapter(locationAdapter);
+        mSpinner.setAdapter(locationAdapter);
 
         extendBtn.setOnClickListener(v -> {
             if (expandableLayout.getVisibility() == View.GONE) {
@@ -67,10 +79,69 @@ public class VacuumView extends DeviceView {
                 expandableLayout.setVisibility(View.GONE);
             }
         });
+
+        final String[] location = getResources().getStringArray(R.array.vacuum_location);
+
+        mSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> arg0, View arg1,
+                                       int arg2, long arg3) {
+                setLocation(location[arg2]);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> arg0) {
+            }
+        });
+        mStateGroup.addOnButtonCheckedListener((group, checkedId, isChecked) -> {
+            if (isChecked) {
+                if (checkedId == R.id.on_button)
+                    start();
+                else if (checkedId == R.id.charge_button)
+                    dock();
+                else
+                    pause();
+            }
+        });
+        mModeGroup.addOnButtonCheckedListener((group, checkedId, isChecked) -> {
+            if (isChecked) {
+                if (checkedId == R.id.aspirar_button)
+                    setMode("vacuum");
+                else
+                    setMode("mop");
+            }
+        });
+
     }
 
     @Override
     public void onDeviceRefresh(Device device) {
 
+        VacuumState state = (VacuumState) device.getState();
+
+        mDevName.setText(getParsedName(device.getName()));
+        mState.setText(getResources().getString(R.string.state,
+                state.getStatus().equals("active")? getResources().getString(R.string.activa) :state.getStatus().equals("docked")?
+                        getResources().getString(R.string.cargandose) +  getResources().getString(R.string.batery_state, state.getBatteryLevel()) + "%":
+                        getResources().getString(R.string.apagada)));
+
+        mLocation.setText(getResources().getString(R.string.disp_location,
+                getParsedName(device.getRoom().getName()),
+                device.getRoom().getHome().getName()));
+    }
+
+    private void setLocation(String roomid){ executeAction("setLocation",new ArrayList<>(Collections.singletonList(roomid)), this::handleError);}
+
+    private void start(){
+        executeAction("start", this::handleError);
+    }
+    private void pause(){
+        executeAction("pause", this::handleError);
+    }
+    private void dock(){
+        executeAction("dock", this::handleError);
+    }
+    private void setMode(String value){
+        executeAction("setMode", new ArrayList<>(Collections.singletonList(value)), this::handleError);
     }
 }
