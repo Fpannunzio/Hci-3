98b719ca8941e52615058bb4d6de9a31e5db9086
Tobias Brandy <tobiasbrandy@hotmail.com>
2020-06-22 16:02:57 -0300

    Las rutinas se refreshean mas rapido

--- app/src/main/java/com/example/hci_3/view_models/RoutineViewModel.java
+++ app/src/main/java/com/example/hci_3/view_models/RoutineViewModel.java
@@ -5,6 +5,7 @@ import androidx.lifecycle.ViewModel;
 
 import com.example.hci_3.api.ApiClient;
 import com.example.hci_3.api.Routine;
+import com.example.hci_3.repositories.DeviceRepository;
 import com.example.hci_3.repositories.RoutineRepository;
 
 import java.util.List;
@@ -13,6 +14,7 @@ public class RoutineViewModel extends ViewModel {
 
     private Boolean isPolling = false;
     protected static RoutineRepository routineRepository = RoutineRepository.getInstance();
+    protected static DeviceRepository deviceRepository = DeviceRepository.getInstance();
 
     public LiveData<List<Routine>> getRoutines(){
         return routineRepository.getRoutines();
@@ -33,10 +35,14 @@ public class RoutineViewModel extends ViewModel {
     }
 
     public void executeRoutine(Routine routine, Runnable responseHandler, ApiClient.ErrorHandler errorHandler){
-        routineRepository.executeRoutine(routine.getId(), responseHandler, errorHandler);
+        routineRepository.executeRoutine(routine.getId(), () -> {
+            deviceRepository.updateDevices();
+            responseHandler.run();
+        }
+        , errorHandler);
     }
 
     public void executeRoutine(Routine routine, ApiClient.ErrorHandler errorHandler){
-        routineRepository.executeRoutine(routine.getId(), () -> {}, errorHandler);
+        executeRoutine(routine, () -> {}, errorHandler);
     }
 }
